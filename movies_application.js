703cf7c606c31941bce216dab1bd41a7a808f5c2
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-09 09:35:22 -0500

    Display the loading gif and display all the movies from Json with AJAX request

--- movies_application.js
+++ movies_application.js
@@ -33,5 +33,24 @@ $(document).ready(function() {
         },
         body: JSON.stringify({title: "Something Borrowed", rating: "2", genre: "Romance", image:'<img class="image" src="img/something-borrowed.jpeg">'})
     }).then(resp => resp.json()).then(movies => console.log(movies));
+    //Displaying movies from JSON in HTML
+    let movieDisplay = () => {
+        let loading = `<div class="loading"><img src="../img/loading-gif.gif"></div>`;
+        $(".display-movies").html(loading);
+        fetch(url).then(resp => resp.json()).then(movies => {
+            let htmlMovies = "";
+            let html = "";
+            movies.forEach(function (movie) {
+                html += `<option value=${movie.id}>${movie.title}</option>`;
+                htmlMovies += `<div class="image">${movie.image}<div>`;
+                htmlMovies += `<h2 class="title">${movie.title}</h2><div class="genre">${movie.genre}</div>`;
+                htmlMovies += `<div><div class="rating">${movie.rating}</div></div>`;
+                htmlMovies += `</div></div>`;
+            });
+            console.log(movies);
+            $(".display-movies").html(htmlMovies);
+        });
+    };
+    movieDisplay();
 
 });
